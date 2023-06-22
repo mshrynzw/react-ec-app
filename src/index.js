f8cab9e27ae4bcb77ad3e17eec9d7f3e96cec05c
kaoruyuki <kaoruyuki>
2023-06-22 15:35:40 +0900

    [add] url

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from "react-redux";
 import createStore from "./reducks/store/store";
+import {ConnectedRouter} from "connected-react-router";
+import * as History from "history";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-export const store =createStore()
+const history = History.createBrowserHistory()
+export const store = createStore(history)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,14 +1,20 @@
 import {
     createStore as reduxCreateStore,
-    combineReducers
+    combineReducers,
+    applyMiddleware
 } from "redux";
 
 import {UsersReducer} from "../users/reducers";
+import {connectRouter, routerMiddleware} from "connected-react-router";
 
-export default function createStore() {
+export default function createStore(history) {
     return reduxCreateStore(
         combineReducers({
+            router: connectRouter(history),
             users: UsersReducer
-        })
+        }),
+        applyMiddleware(
+            routerMiddleware(history)
+        )
     )
 }
