31cbe45caa7b631b0a42e3b317e35a1af784ccd6
kaoruyuki <kaoruyuki>
2023-06-23 08:26:08 +0900

    [fix] Firebase Authentication

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
 import {firebaseConfig} from './config'
 
 firebase.initializeApp(firebaseConfig)
