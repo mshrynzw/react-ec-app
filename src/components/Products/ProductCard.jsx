1da3aedf71a12846feb1238f7aa3b75c80a1ed9b
kaoruyuki <kaoruyuki>
2023-06-26 20:20:23 +0900

    [add] Edit Button

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, {useState} from 'react'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
@@ -7,6 +7,10 @@ import {makeStyles} from "@material-ui/core/styles"
 import NoImage from '../../assets/img/src/no_image.png'
 import {push} from 'connected-react-router'
 import {useDispatch} from "react-redux"
+import IconButton from "@material-ui/core/IconButton"
+import Menu from "@material-ui/core/Menu"
+import MenuItem from "@material-ui/core/MenuItem"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -61,6 +65,15 @@ const ProductCard = (props) => {
     const classes = useStyles()
     const dispatch = useDispatch()
 
+    const [anchorEl, setAnchorEl] = useState(null)
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
     const images = (props.images.length > 0) ? props.images : [{path: NoImage}]
     const price = props.price.toLocaleString()
 
@@ -73,9 +86,8 @@ const ProductCard = (props) => {
             />
             <CardContent
                 className={classes.content}
-                onClick={() => dispatch(push(`/product/${props.id}`))}
             >
-                <div>
+                <div onClick={() => dispatch(push(`/product/${props.id}`))}>
                     <Typography color="textSecondary" component="p">
                         {props.name}
                     </Typography>
@@ -83,6 +95,29 @@ const ProductCard = (props) => {
                         ￥{price}
                     </Typography>
                 </div>
+                <IconButton onClick={handleClick}>
+                    <MoreVertIcon/>
+                </IconButton>
+                <Menu
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem
+                        onClick={() => {
+                            dispatch(push(`/product/edit/${props.id}`))
+                            handleClose()
+                        }}
+                    >
+                        編集する
+                    </MenuItem>
+                    <MenuItem
+
+                    >
+                        削除する
+                    </MenuItem>
+                </Menu>
             </CardContent>
         </Card>
     )
